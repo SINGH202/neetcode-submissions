a50eb61924c327d27f652d28b92e3cff606f0509
ANURAG SINGH <apk.anurag.singh@gmail.com>
2026-06-08 18:03:35 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length){
+            return false
+        }
+        //sorting
+        const sortS = s.split("").sort().join("");
+        const sortT = t.split("").sort().join("");
+        if(sortS !== sortT){
+            return false;
+        }
+        return true; 
+
+        
+    }
+}
